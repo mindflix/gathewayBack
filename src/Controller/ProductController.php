d6f4c07a08b6028b3d50f03de1e3996ac613012c
mindflix <nyco.demol@gmail.com>
2021-02-09 00:45:20 +0100

    YYESSSSS

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,19 @@
 
 namespace App\Controller;
 
-use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 
 
 /**
- * @Route("/user",name="user")
+ * @Route("/product",name="product")
  */
-class UserController extends AbstractFOSRestController
+class ProductController extends AbstractFOSRestController
 {
     /**
      * @Rest\Get("/")
